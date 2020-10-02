f545eaecdd6f40725c639607fb731741213ebb07
Aubrey Portwood <aubreypwd@users.noreply.github.com>
2020-10-02 12:50:35 -0600

    Update and rename zsh-plugin-NAME.plugin.zsh to zsh-plugin-vcshr.plugin.zsh

---
+++ zsh-plugin-vcshr.plugin.zsh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+if [[ $(command -v antigen) ]]; then
+	#antigen bundle aubreypwd/zsh-plugin-require@1.0.1
+	#antigen apply
+
+	#require "{BREWCOMMAND}" "brew reinstall {BREWCOMMAND}" "brew"
+fi
+
+###
+ #
+ #
+ # @since 1.0.0
+ ##
+
+function vcshr {
+}
