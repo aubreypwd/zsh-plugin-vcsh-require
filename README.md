06449f7a361594a9a33ffe400b7f89bb66ae1723
Aubrey Portwood <code@aubreypwd.com>
2020-10-02 13:43:34 -0600

    Details.

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
-# ``
+# `vcshr`
 
-Description
+A function to help `vcsh` users _require_ *Github* repositories using `vcsh` for auto-installation in `~/.zshrc`, etc.
 
 ## Usage
 
 ```bash
+vcshr "<packag-name>" "<github-username>" "<github-repo>" --overwrite --autoignore --ssh
 ```
+
+E.g.:
+
+```bash
+vcshr "homebrew" "aubreypwd" "vcsh-homebrew" --overwrite --autoignore --ssh
+```
+
+Will require `git@github.com:aubreypwd/vcsh-homebrew.git` as `homebrew`.
+
+## Argumentes
+
+- `--overwrite` to make sure that all files get clobbered by the clone process
+- `--autoignore` to automatically write a new `.gitignore.d` for the repo
+- `--ssh` to use Github's SSH URL vs. HTTPS (default)
 
 ## Requires
 
-- `...`<sup>*</sup>
+- `vcsh`<sup>*</sup>
 
 <sup>*</sup> If using [homebrew](https://brew.sh) on macOS, these will automatically be installed.
 
@@ -18,7 +33,7 @@ Description
 Using [antigen](https://github.com/zsh-users/antigen):
 
 ```bash
-antigen bundle aubreypwd/zsh-plugin-NAME@1.0.0
+antigen bundle aubreypwd/zsh-plugin-vcshr@1.0.0
 ```
 
 ## Development
@@ -26,10 +41,10 @@ antigen bundle aubreypwd/zsh-plugin-NAME@1.0.0
 Install the package on `master`:
 
 ```bash
-antigen bundle aubreypwd/zsh-plugin-NAME
+antigen bundle aubreypwd/zsh-plugin-vcshr
 ```
 
-...and contribute upstream by working in `$HOME/.antigen/bundles/aubreypwd/zsh-plugin-NAME`.
+...and contribute upstream by working in `$HOME/.antigen/bundles/aubreypwd/zsh-plugin-vcshr`.
 
 ---
 
--- zsh-plugin-vcshr.plugin.zsh
+++ zsh-plugin-vcshr.plugin.zsh
@@ -1,17 +1,54 @@
 #!/bin/zsh
 
 if [[ $(command -v antigen) ]]; then
-	#antigen bundle aubreypwd/zsh-plugin-require@1.0.1
-	#antigen apply
+	antigen bundle aubreypwd/zsh-plugin-require@1.0.1
+	antigen apply
 
-	#require "{BREWCOMMAND}" "brew reinstall {BREWCOMMAND}" "brew"
+	require "vcsh" "brew reinstall vcsh" "brew"
 fi
 
 ###
+ # vcshr
  #
+ # E.g. vcshr "homebrew" "aubreypwd" "vcsh-homebrew" --overwrite --autoignore
+ #
+ # This will require http://github.com/
  #
  # @since 1.0.0
+ # @since 10/2/20
+ #
+ # @author Aubrey Portwood <code@aubreypwd.com>
  ##
-
 function vcshr {
+	if ! [[ -x $(command -v vcsh) ]]; then >&2 echo "This uses the command 'vcsh' and we could not find the command." && return; fi
+
+	local name="$1"
+	local username="$2"
+	local repo="$3"
+
+	if [[ $(vcsh list) == *"$name"* ]]; then
+		echo "$name already exists:"
+		vcsh list
+		return; # You already have it in your list, fail gracefully.
+	fi
+
+	local repo_url="https://github.com/$username/$repo" # Default to https, unless they pass the --ssh flag.
+
+	if [[ "$@" == *"--ssh"* ]]; then
+		local repo_url="ssh://git@github.com/$username/$repo.git"
+	fi
+
+	vcsh clone "$repo_url" "$name"
+
+	if [[ "$@" == *"--overwrite"* ]]; then
+		vcsh "$name" reset --hard origin/master
+	fi
+
+	if [[ "$@" == *"--autoignore"* ]]; then
+		sleep 2
+
+		# Yes, we have to do it twice
+		vcsh write-gitignore "$name" # Once to make the backup file, but it exists.
+		vcsh write-gitignore "$name" # Again, to write the new ignore file because the first one exited.
+	fi
 }
